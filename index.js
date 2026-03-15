b65cfe1d36fbfad1492da62cc00e1077bbf585c2
amareshbose4321-git <amareshbose4321@gmail.com>
2026-03-15 09:57:45 +0530

    Update file

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// console.log() is a JavaScript function used to print messages
+// in the browser's Developer Console.
+
+// "Hello World!!!" is a string (text) that we want to display.
+
+// When this line runs, it will show "Hello World!!!"
+// in the browser console (not directly on the webpage).
+
 console.log("Hello World!!!");
+
 console.log("Amaresh");
-console.log("Happy");
+
+console.log("Happy!!!");
